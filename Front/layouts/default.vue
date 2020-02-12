570abf5942506d87d559b5783ba94413aa6a5037
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2020-02-12 15:08:50 +0900

    Create Router in Nuxt

---
+++ Front/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Nuxt의 라우터 설정 -->
+  <div>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/profile">profile</nuxt-link>
+      <nuxt-link to="signup">SignUp</nuxt-link>
+    </nav>
+
+    <!-- pages에서 호출하는 페이지 -->
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- Front/pages/index.vue
+++ Front/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Index Page
+    Main Page
   </div>
 </template>
 
