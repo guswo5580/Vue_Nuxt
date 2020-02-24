4dbd2e9264141f8725436a32c1ab2572a0561004
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2020-02-24 12:48:11 +0900

    Add infinite scrolling using limit

--- Front_Tech/pages/index.vue
+++ Front_Tech/pages/index.vue
@@ -8,52 +8,68 @@
 </template>
 
 <script>
-  import PostCard from '~/components/PostCard';
-  import PostForm from '~/components/PostForm';
+import PostCard from "~/components/PostCard";
+import PostForm from "~/components/PostForm";
 
-  export default {
-    components: {
-      PostCard,
-      PostForm,
+export default {
+  components: {
+    PostCard,
+    PostForm
+  },
+  data() {
+    return {
+      name: "Nuxt.js"
+    };
+  },
+  //state의 값을 꺼내오기
+  computed: {
+    me() {
+      return this.$store.state.users.me;
     },
-    data() {
-      return {
-        name: 'Nuxt.js',
-      };
+    mainPosts() {
+      return this.$store.state.posts.mainPosts;
     },
-    computed: {
-      me() {
-        return this.$store.state.users.me;
-      },
-      mainPosts() {
-        return this.$store.state.posts.mainPosts;
-      },
-      hasMorePost() {
-        return this.$store.state.posts.hasMorePost;
-      }
-    },
-    fetch({ store }) {
-      store.dispatch('posts/loadPosts');
-    },
-    mounted() {
-      window.addEventListener('scroll', this.onScroll);
-    },
-    beforeDestroy() {
-      window.removeEventListener('scroll', this.onScroll);
-    },
-    methods: {
-      onScroll() {
-        console.log('scroll');
-        if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
-          if (this.hasMorePost) {
-            this.$store.dispatch('posts/loadPosts');
-          }
+    hasMorePost() {
+      return this.$store.state.posts.hasMorePost;
+    }
+  },
+
+  //화면이 렌더링 되기 전 fetch를 통해 Post요청
+  fetch({ store }) {
+    store.dispatch("posts/loadPosts");
+  },
+
+  //화면 부착 시!!!, created로 사용하지 않는 것에 주의
+  mounted() {
+    //스크롤 인식!!
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    //제거를 통해 메모리 누수 방지
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      console.log("scroll");
+      if (
+        //window.scrollY = 현재 화면의 Y축 계산 --- scroll을 Y축으로 얼마나 움직였는가
+        //clientHeight = 페이지 최상단 ~ 화면에 표시된 페이지 최하단
+        //scrollHeight = 페이지 최상단 ~ 렌더링된 화면 전체의 페이지 최하단
+        //렌더링된 화면 전체의 페이지 최하단 시!!
+        //scrollY + clientHeight = scrollHeight
+        //따라서, 최하단이 아닌 중간지점일 경우
+        //scrollY + clientHeight < scrollHeight
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+        //scroll은 === 를 통해 비교하면 정확하지 않음!! 따라서 부등호를 이용
+      ) {
+        if (this.hasMorePost) {
+          this.$store.dispatch("posts/loadPosts");
         }
-      },
-    },
-  };
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- Front_Tech/store/posts.js
+++ Front_Tech/store/posts.js
@@ -1,8 +1,12 @@
 export const state = () => ({
+  //게시글 정보
   mainPosts: [],
-  hasMorePost: true,
+  //인피니티 스크롤링 여부 확인
+  hasMorePost: true
 });
 
+//예시 데이터 설정(Dummy) - Front는 limit, totalPosts 등을 알 필요 없음!!
+//따라서 state에 넣지 않는다!!!
 const totalPosts = 51;
 const limit = 10;
 
@@ -18,37 +22,58 @@ export const mutations = {
     const index = state.mainPosts.findIndex(v => v.id === payload.postId);
     state.mainPosts[index].Comments.unshift(payload);
   },
+
+  //게시글 불러오기
   loadPosts(state) {
-    const diff = totalPosts - state.mainPosts.length; // 아직 안 불러온 게시글 수
-    const fakePosts = Array(diff > limit ? limit : diff).fill().map(v => ({
-      id: Math.random().toString(),
-      User: {
-        id: 1,
-        nickname: '제로초',
-      },
-      content: `Hello infinite scrolling~ ${Math.random()}`,
-      Comments: [],
-      Images: [],
-    }));
-    state.mainPosts = state.mainPosts.concat(fakePosts);
+    //totalPost = 전체 게시글 수, mainPosts.length = 현재 내가 불러온 post 개수
+    //diff = 아직 불러오지 않은 게시글 수
+    const diff = totalPosts - state.mainPosts.length;
+
+    //Array = 배열 생성, fill = 배열 채우기, map = 배열 내 삽입하기
+    const fakePosts = Array(diff > limit ? limit : diff)
+      .fill()
+      .map(v => ({
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: "예시"
+        },
+        content: `Hello infinite scrolling~ ${Math.random()}`,
+        Comments: [],
+        Images: []
+      }));
+
+    //불러오지 않은 게시글이 limit보다 작으면 diff 만큼의 수를 불러옴
+    //diff만큼을 불러왔을 때 fakePosts.length < limit
+    //따라서 hasMorePost === false로 바뀜
     state.hasMorePost = fakePosts.length === limit;
-  },
+
+    //기존 mainPosts와 새로 삽입되는 Posts 배열 합치기
+    //concat = 배열 합치기
+    state.mainPosts = state.mainPosts.concat(fakePosts);
+  }
 };
 
 export const actions = {
   add({ commit }, payload) {
     // 서버에 게시글 등록 요청 보냄
-    commit('addMainPost', payload);
+    commit("addMainPost", payload);
   },
   remove({ commit }, payload) {
-    commit('removeMainPost', payload);
+    commit("removeMainPost", payload);
   },
   addComment({ commit }, payload) {
-    commit('addComment', payload);
+    commit("addComment", payload);
   },
+
+  //게시글 불러오기
   loadPosts({ commit, state }, payload) {
     if (state.hasMorePost) {
-      commit('loadPosts');
+      commit("loadPosts");
     }
   }
 };
+
+//limit 기반을 실무에 사용하지 않는 이유
+//1. 서버의 응답이 늦어진 경우, 무한 요청이 일어날 경우가 생김!!
+//2. 삭제, 갱신 등을 통해 totalPosts가 바뀌는 경우가 생김!!
